c19225676312050ceb7bf2759a96917f363eae7b
ROCKTAKEY <rocktakey@gmail.com>
2020-12-06 23:12:23 +0900

    First commit.
    Remove cursor-test.

---
+++ ivy-migemo.el
@@ -0,0 +1,37 @@
+;;; ivy-migemo.el --- Use migemo on ivy              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: matching
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1") (ivy "0.13.0"))
+
+;; URL: https://github.com/ROCKTAKEY/ivy-migemo
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defgroup ivy-migemo ()
+  "Group for ivy-migemo."
+  :group 'ivy
+  :prefix "ivy-migemo-")
+
+(provide 'ivy-migemo)
+;;; ivy-migemo.el ends here
