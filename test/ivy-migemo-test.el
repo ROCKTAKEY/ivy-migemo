c6578a07ea1a58598d785b13edca49a1dc1ce4ce
ROCKTAKEY <rocktakey@gmail.com>
2020-12-07 15:56:38 +0900

    Add test.

--- test/ivy-migemo-test.el
+++ test/ivy-migemo-test.el
@@ -36,5 +36,143 @@
 
 (require 'ert)
 
+(migemo-init)
+
+(defun test-ivy--filter (regex-seq string)
+  (ivy--re-filter regex-seq (list string)))
+
+;; regex
+(ert-deftest ivy-migemo--regex-hiragana ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex "aiueo")
+           "あいうえお"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex "aiueo")
+           "I am あいうえお"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex "aiueo kakikukeko")
+           "あいうえお is かきくけこ"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex "aiueo kakikukeko")
+               "あいうえお is さしすせそ")))
+
+(ert-deftest ivy-migemo--regex-katakana ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex "aiueo")
+           "アイウエオ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex "aiueo")
+           "I am アイウエオ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex "aiueo kakikukeko")
+           "アイウエオ is カキクケコ")))
+
+(ert-deftest ivy-migemo--regex-kanji ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex "saikou")
+           "最高"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex "saikou")
+           "I am 最高"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex "saikou saitei")
+           "最高 最低"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex "saikou teigaku")
+               "最高 最低")))
+
+(ert-deftest ivy-migemo--regex-plus-hiragana ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo")
+           "あいうえお"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo")
+           "I am あいうえお"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo kakikukeko")
+           "あいうえお is かきくけこ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo ! kakikukeko")
+           "あいうえお さしすせそ"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-plus "aiueo ! kakikukeko")
+               "あいうえお is かきくけこ")))
+
+(ert-deftest ivy-migemo--regex-plus-katakana ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo")
+           "アイウエオ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo")
+           "I am アイウエオ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo kakikukeko")
+           "アイウエオ is カキクケコ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "aiueo ! kakikukeko")
+           "アイウエオ サシスセソ"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-plus "aiueo ! kakikukeko")
+               "アイウエオ is カキクケコ")))
+
+(ert-deftest ivy-migemo--regex-plus-kanji ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "saikou")
+           "最高"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "saikou")
+           "I am 最高"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "saikou saitei")
+           "最高 最低"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-plus "saikou teigaku")
+               "最高 最低"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-plus "saikou ! teigaku")
+           "最高 最低"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-plus "saikou ! saitei")
+               "最高 is 最低")))
+
+;; fuzzy
+(ert-deftest ivy-migemo--regex-fuzzy-hiragana ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex-fuzzy "aiueo")
+           "あいうえお"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-fuzzy "aiueo")
+           "I am あいうえお"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-fuzzy "aiueo kakikukeko")
+               "あいうえお is かきくけこ")))
+
+(ert-deftest ivy-migemo--regex-fuzzy-katakana ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex-fuzzy "aiueo")
+           "アイウエオ"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-fuzzy "aiueo")
+           "I am アイウエオ"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-fuzzy "aiueo kakikukeko")
+               "アイウエオ is カキクケコ")))
+
+(ert-deftest ivy-migemo--regex-fuzzy-kanji ()
+  (should (test-ivy--filter
+           (ivy-migemo--regex-fuzzy "saikou")
+           "最高"))
+  (should (test-ivy--filter
+           (ivy-migemo--regex-fuzzy "saikou")
+           "I am 最高"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-fuzzy "saikou saitei")
+               "最高 最低"))
+  (should-not (test-ivy--filter
+               (ivy-migemo--regex-fuzzy "saikou saitei")
+               "最高 is 最低")))
+
+
+
 (provide 'ivy-migemo-test)
 ;;; ivy-migemo-test.el ends here
