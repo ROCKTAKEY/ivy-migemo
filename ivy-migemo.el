9dec27e3ce73227afd85b516248c23594997f613
ROCKTAKEY <rocktakey@gmail.com>
2023-01-22 03:43:52 +0900

    docs: Update README and commentary

--- ivy-migemo.el
+++ ivy-migemo.el
@@ -73,15 +73,15 @@
 ;;   |
 ;;   | ;; If you want to defaultly use migemo on swiper and counsel-find-file:
 ;;   | (setq ivy-re-builders-alist '((t . ivy--regex-plus)
-;;   |                               (swiper . ivy-migemo--regex-plus)
-;;   |                               (counsel-find-file . ivy-migemo--regex-plus))
-;;   |                               ;(counsel-other-function . ivy-migemo--regex-plus)
+;;   |                               (swiper . ivy-migemo-regex-plus)
+;;   |                               (counsel-find-file . ivy-migemo-regex-plus))
+;;   |                               ;(counsel-other-function . ivy-migemo-regex-plus)
 ;;   |                               )
 ;;   | ;; Or you prefer fuzzy match like ido:
 ;;   | (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)
-;;   |                               (swiper . ivy-migemo--regex-fuzzy)
-;;   |                               (counsel-find-file . ivy-migemo--regex-fuzzy))
-;;   |                               ;(counsel-other-function . ivy-migemo--regex-fuzzy)
+;;   |                               (swiper . ivy-migemo-regex-fuzzy)
+;;   |                               (counsel-find-file . ivy-migemo-regex-fuzzy))
+;;   |                               ;(counsel-other-function . ivy-migemo-regex-fuzzy)
 ;;   |                               )
 ;;   `----
 
@@ -94,7 +94,7 @@
 
 ;;   Toggle fuzzy match or not on ivy.  Almost same as `ivy-toggle-fuzzy',
 ;;   except this function can also be used to toggle between
-;;   `ivy-migemo--regex-fuzzy' and `ivy-migemo--regex-plus'.
+;;   `ivy-migemo-regex-fuzzy' and `ivy-migemo-regex-plus'.
 
 
 ;; 3.2 `ivy-migemo-toggle-migemo'
